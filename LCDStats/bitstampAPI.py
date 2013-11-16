89decb632aaab1556a1e2590557040e0d5fe7678
labmixz <labmixz@gmail.com>
2013-11-15 19:06:31 -0500

    test

--- LCDStats/bitstampAPI.py
+++ LCDStats/bitstampAPI.py
@@ -40,6 +40,7 @@ class bitstampAPI:
 
 
 	def makereq(self, path, data):
+		# bare-bones hmac rest sign
 		return urllib2.Request(self.base)
 
 
@@ -56,9 +57,8 @@ class bitstampAPI:
 						
 			except urllib2.HTTPError as e:
 				response = e.fp
-				print response
-				
-		self.lastBitstamp = json.load(response)
+				self.lastBitstamp = json.load(response)
+		print json.dumps(self.lastBitstamp, sort_keys=True, indent=4, separators=(',', ': '))
 		return self.lastBitstamp
 
 
@@ -68,9 +68,11 @@ if __name__=='__main__':
 	while True:
 		try:			
 			new_bid_price = bitstamp.req('BTCUSD/money/ticker_fast', {}, True) 
+			#print json.dumps(new_bid_price, sort_keys=True, indent=4, separators=(',', ': '))
 			if new_bid_price:
 				bid_price = new_bid_price
-	
-			print bid_price['last']
+			print json.dumps(bid_price, sort_keys=True, indent=4, separators=(',', ': '))
+			#print bid_price['last']
+			#print "Current USD Bid Price: %f" % (bid_price['data']['amount'] / 1e5)
 		except Exception as e:
 			print "Error - %s" % e
